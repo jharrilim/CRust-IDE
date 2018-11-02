65ed363b67cdd4372bad243ce31f83e9e2b91920
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-11-02 16:46:20 -0400

    add file explorer and editor components

---
+++ CRust.Api/Editor/FileLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace CRust.Api.Editor
+{
+    public static class FileLoader
+    {
+        public static async Task<string> GetFileAsStringAsync(string path)
+        {
+            string txt = "";
+            using (StreamReader reader = new StreamReader(path))
+            {
+                txt = await reader.ReadToEndAsync();
+            }
+            return txt;
+        }
+    }
+}
