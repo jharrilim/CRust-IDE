d4b7f448a386e0add6ddf0d88a95349465711635
Joseph Harrison-Lim <josephharrisonlim@gmail.com>
2018-11-03 01:07:48 -0400

    add run logic

--- CRust.Api/Cargo/Runner.cs
+++ CRust.Api/Cargo/Runner.cs
@@ -1,31 +1,74 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace CRust.Api.Cargo
 {
     public sealed class Runner
     {
-        private const string defaultPath = "~/.cargo/bin";
+        private const string cargoDirName = ".cargo/bin";
+        private const string cargo = "cargo";
 
-        public string RustPath { get; set; }
+        private readonly string cargoDirPath;
 
-        public Runner() { }
+        private StreamReader stdout;
 
-        public void Build()
+        public Runner()
         {
-
+            cargoDirPath = Path.Combine
+            (
+                Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+                cargoDirName
+            );
         }
 
-        public void Run()
+        public void Run(string cmd, out string stdout, out string stderr)
         {
+            StringBuilder sbOut = new StringBuilder();
+            StringBuilder sbErr = new StringBuilder();
 
-        }
+            ProcessStartInfo processStartInfo = new ProcessStartInfo
+            {
+                CreateNoWindow = true,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                Arguments = cmd,
+                FileName = cargo
+            };
 
-        public void Install()
-        {
+            Process process = new Process
+            {
+                StartInfo = processStartInfo,
+                EnableRaisingEvents = true
+            };
 
+            process.OutputDataReceived += new DataReceivedEventHandler
+            (
+                (object sender, DataReceivedEventArgs e) =>
+                {
+                    sbOut.Append(e.Data);
+                }
+            );
+
+            process.ErrorDataReceived += new DataReceivedEventHandler
+            (
+                (object sender, DataReceivedEventArgs e) =>
+                {
+                    sbErr.Append(e.Data);
+                }
+            );
+
+            process.Start();
+            process.BeginOutputReadLine();
+            process.WaitForExit();
+            process.CancelOutputRead();
+
+            stdout = sbOut.ToString();
+            stderr = sbErr.ToString();
         }
     }
 }
